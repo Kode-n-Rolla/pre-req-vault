b54833b856352a247db05107d5bfa7838eaa05ba
kode-n-rolla <pavlucio.auditore@gmail.com>
2026-08-18 17:37:41 -0300

    formatting

--- programs/pre-req-vault/src/instructions/close.rs
+++ programs/pre-req-vault/src/instructions/close.rs
@@ -10,18 +10,18 @@ pub struct Close<'info> {
     pub user: Signer<'info>,
 
     #[account(
-    mut,
-    seeds = [b"vault", vault_state.key().as_ref()],
-    bump = vault_state.vault_bump,
-  )]
+        mut,
+        seeds = [b"vault", vault_state.key().as_ref()],
+        bump = vault_state.vault_bump,
+    )]
     pub vault: SystemAccount<'info>,
 
     #[account(
-    mut,
-    seeds = [b"state", user.key().as_ref()],
-    bump = vault_state.state_bump,
-    close = user,
-  )]
+        mut,
+        seeds = [b"state", user.key().as_ref()],
+        bump = vault_state.state_bump,
+        close = user,
+    )]
     pub vault_state: Account<'info, VaultState>,
 
     system_program: Program<'info, System>,
